3a198908162997e77d5118f653833d222852c2f3
amcourson <amcourson09@sbcglobal.net>
2021-03-19 22:33:58 -0500

    adding app.get

--- Develop/server.js
+++ Develop/server.js
@@ -6,13 +6,21 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Star Wars Page!');
+    res.sendFile(path.join(__dirname,"./public/js/index.js"));
   });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,"./public/notes.html"));
   });
 
+  app.get('api/notes', (req, res) => {
+    
+  });
 
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 
 
